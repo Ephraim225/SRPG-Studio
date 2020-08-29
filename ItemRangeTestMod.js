9d08c3ab67f4508b9ef516b039123208a07ab6a7
Ephraim225 <58414822+Ephraim225@users.noreply.github.com>
2020-08-28 21:40:24 -0700

    Update ItemRangeTestMod.js

--- ItemRangeTestMod.js
+++ ItemRangeTestMod.js
@@ -1,5 +1,6 @@
 //Function modified by Ephraim225. WORK IN PROGRESS.
 //The idea is to customize the tiles you can use an item/weapon on. So far it works for the player but the AI is not restricted in the same fashion.
+// 8/28/2020 Made a step forward. If the item is in the enemy's inventory it will now move and use the item ONLY if a unit is already in range from its starting position BUT it's still not under the correct restrictions when it moves and uses the item.
 	IndexArray.createIndexArray = function(x, y, item) {
 		var i, rangeValue, rangeType, arr;
 		var startRange = 1;
@@ -62,4 +63,72 @@
 		}
 		
 		return this.getBestIndexArray(x, y, startRange, endRange);
+	}
+	
+//For getting the AI to obey
+	AIScorer.Item.getScore = function(unit, combination) {
+		var obj;
+		var item = combination.item;
+		var score = 0;
+		var illegal = 1;
+		
+		activeX = unit.getMapX();
+		activeY = unit.getMapY();
+		passiveX = combination.targetUnit.getMapX();
+		passiveY = combination.targetUnit.getMapY();
+		targettile = passiveX + passiveY * CurrentMap.getWidth(); //Target's position
+		start = activeX + activeY * CurrentMap.getWidth(); //User's position
+		
+		if (item === null || item.isWeapon()) {
+			return score;
+		}
+		obj = ItemPackageControl.getItemAIObject(item);
+		if (obj === null) {
+			return score;
+		}
+		
+	//Check for unit's position in relation to target here. First create an array with the legal tiles available.
+	if (item.custom.range != null && !item.isWeapon()) {
+			// {range: "Row"}
+			if (item.custom.range == "Row") {
+			count = item.getRangeValue();
+			height = CurrentMap.getWidth();
+			//start = combination.posIndex;
+			
+			arr = [];
+			arr.length = count*4;
+			for (i = 0; i < count; i++) {
+					j = i*4
+					arr[j] = start+(i+1);
+					arr[j+1] = start-(i+1);
+					arr[j+2] = start+height*(i+1);
+					arr[j+3] = start-height*(i+1);
+				}
+		}
+	
+	//We've created an array with all legal tiles for using the item. If the target unit is is in a legal tile do nothing.
+	arraylen = arr.length
+	for (i = 0; i < arraylen; i++) {
+		if (arr[i] == targettile) {
+			illegal = 0;
+			//score = 1;
+			root.log("That's legal!");
+		}
+	}
+	if (illegal == 1){
+		root.log("Illegal item use attempt: From " + String(activeX) + "," + String(activeY) + " to " + String(passiveX) + "," + String(passiveY));
+		//score = -99;
+		return -1;
+	}
+		
+	}
+	
+	//Finish
+	
+		score = obj.getItemScore(unit, combination);
+		if (score < 0) {
+			return -1;
+		}
+	
+		return score + this._getPlusScore(unit, combination);
 	}
