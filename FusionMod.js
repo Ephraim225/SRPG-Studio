97fca746329cf5cff7e578ee091dd0c39494e164
Ephraim225 <58414822+Ephraim225@users.noreply.github.com>
2020-10-17 01:15:40 -0700

    Update FusionMod.js

--- FusionMod.js
+++ FusionMod.js
@@ -1,5 +1,9 @@
 //Made by Ephraim225.
-//Literally the only thing this plugin does is comment out the bit of code that prevents CPU units from being able to do Fusions. The AI won't do it naturally, but they can do Fusions in events.
+//Literally the only thing the first part of the plugin does is comment out the bit of code that prevents CPU units from being able to do Fusions.
+//The AI won't do it naturally, but they can do Fusions in events.
+//NEW: Give an enemy a custom skill with the keyword "CapSkill" to leave them at one health and add a State when you kill them.
+//To determine which state, set the custom parameter "addstate" to the ID of the state you want.
+//You can use this to trigger an event on the unit when they have whatever state you specified.
 
 CatchFusionAction.setFusionParam = function(fusionParam) {
 		var directionArray = [DirectionType.RIGHT, DirectionType.BOTTOM, DirectionType.LEFT, DirectionType.TOP];
@@ -21,4 +25,30 @@ CatchFusionAction.setFusionParam = function(fusionParam) {
 		}
 		
 		return true;
+	}
+	
+DamageControl.checkHp = function(active, passive) {
+		var hp = passive.getHp();
+		var skill = SkillControl.getPossessionCustomSkill(active, "CapAttack")
+		
+		if (hp > 0) {
+			return;
+		}
+		
+		//New part of the code here.
+		if (skill !== null && skill.custom.addstate !== null) {
+			var list = root.getBaseData().getStateList();
+			var status = list.getDataFromId(skill.custom.addstate);
+			StateControl.arrangeState(passive, status, 0);
+			passive.setHp(1);
+			return;
+		}
+		
+		if (FusionControl.getFusionAttackData(active) !== null) {
+			// For isLosted which will be called later, hp doesn't become 1 at this moment.
+			this.setCatchState(passive, false);
+		}
+		else {
+			this.setDeathState(passive);
+		}
 	}
