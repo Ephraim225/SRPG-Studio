27daeb1095d6e53603ae3139ae5667049301076f
Ephraim225 <58414822+Ephraim225@users.noreply.github.com>
2022-04-16 17:36:33 -0700

    Update AbsorbHPModifier.js
    Fixed, now it works

--- AbsorbHPModifier.js
+++ AbsorbHPModifier.js
@@ -1,58 +1,48 @@
 //By Ephraim225. This one is simple; it allows you to customize the Absorb HP effect to absorb a custom amount of health.
-//On either your weapon or skill that absorbs HP, enter the custom parameter "absorbrate" to set a percentage of damage inflicted to increase the attacker's HP by. (Make it a decimal value, as in 0.5 means 50%)
-//Another customparameter is "absorbflat". This will restore a flat amount of health instead. If you input both, you absorb whichever value ends up higher.
+//On either your weapon or skill that absorbs HP, enter the following custom parameters:
+// "absorbrate" makes it absorb a percentage of the damage inflicted. Input a decimal value, such as 0.5 for 50% absorption.
+// "absorbflat" makes it restore a fixed amount of HP even if damage inflicted was lower.
 
-AttackEvaluator._arrangeActiveDamage = function(virtualActive, virtualPassive, attackEntry) {
+//If both parameters are present, the higher amount will be restored.
+//If multiple sources of HP absorb are present on the unit, the highest possible amount of HP between them will be absorbed.
+
+// Unlike the other parameters, if this is present on a unit's weapon AND skill, the skill's parameter always overrides it.
+
+AttackEvaluator.ActiveAction._arrangeActiveDamage = function(virtualActive, virtualPassive, attackEntry) {
 		var max;
 		var active = virtualActive.unitSelf;
-		var damageActive = attackEntry.damageActive;
-		var damagePassive = attackEntry.damagePassive;
-		var weapon = virtualActive.weapon;
-		var healthrestore = this._isAbsorption(virtualActive, virtualPassive, attackEntry);
-		
-		if (healthrestore > 0) {
-			max = ParamBonus.getMhp(active);
-			
-			damageActive = healthrestore;
-			
-			if (virtualActive.hp + damageActive > max) {
-				damageActive = max - virtualActive.hp;
-			}
-			
-			// If damage is minus, it means recovery.
-			damageActive *= -1;
-		}
-		else {
-			damageActive = 0;
-		}
-		
-		return damageActive;
-	}
-	
-AttackEvaluator._isAbsorption = function(virtualActive, virtualPassive, attackEntry) {
-		var isWeaponAbsorption;
-		var active = virtualActive.unitSelf;
 		var passive = virtualPassive.unitSelf;
+		var damageActive = attackEntry.damageActive;
+		var damagePassive = this._arrangePassiveDamage(virtualActive, virtualPassive, attackEntry);
 		var weapon = virtualActive.weapon;
-		var percentage = 0;
+		var percentage = 1;
 		var flatbonus = 0;
-		var percentage2 = 0;
+		var percentage2 = 1;
 		var flatbonus2 = 0;
 		var healthrestore = 0;
-		var damageActive = attackEntry.damageActive;
+		//var multiplier = 1;
 		var skillcontrol = SkillControl.checkAndPushSkill(active, passive, attackEntry, true, SkillType.DAMAGEABSORPTION);
 		
 		if (weapon !== null && weapon.getWeaponOption() === WeaponOption.HPABSORB) {
-			percentage = Math.floor(damageActive * (weapon.custom.absorbrate !== null));
-			flatbonus = weapon.custom.absorbflat !== null;
-		}
-		else {
-			isWeaponAbsorption = false;
+			if (weapon.custom.absorbrate !== null) {
+			percentage = Math.floor(damagePassive * weapon.custom.absorbrate);
+			}
+			if (weapon.custom.absorbflat !== null) {
+			flatbonus = weapon.custom.absorbflat;
+			}
+			//root.log("Percent:" + percentage)
+			//root.log("Flat:" + flatbonus)
 		}
 		
 		if (skillcontrol !== null) {
-			percentage2 = Math.floor(damageActive * (skillcontrol.custom.absorbrate !== null));
-			flatbonus2 = skillcontrol.custom.absorbflat !== null;
+			if (skillcontrol.custom.absorbrate !== null) {
+			percentage2 = Math.floor(damagePassive * skillcontrol.custom.absorbrate);
+			}
+			if (skillcontrol.custom.absorbflat !== null) {
+			flatbonus2 = skillcontrol.custom.absorbflat;
+			}
+			//root.log("Percent:" + percentage2)
+			//root.log("Flat:" + flatbonus2)
 		}
 		
 		if (healthrestore < percentage) {
@@ -68,5 +58,33 @@ AttackEvaluator._isAbsorption = function(virtualActive, virtualPassive, attackEn
 			healthrestore = flatbonus2;
 		}
 		
-		return healthrestore;
+		if (healthrestore > 0) {
+			max = ParamBonus.getMhp(active);
+			
+			damageActive = healthrestore;
+			
+			if (virtualActive.hp + damageActive > max) {
+				damageActive = max - virtualActive.hp;
+			}
+			
+			// If damage is minus, it means recovery.
+			
+			//Not working ugh
+			//if (weapon.custom.multiplier !== null) {
+			//multiplier = weapon.custom.multiplier;
+			//}
+			
+			//if (skillcontrol.custom.multiplier !== null) {
+			//multiplier = skillcontrol.custom.multiplier;
+			//}
+
+			damageActive *= -1;
+			//damageActive *= multiplier;
+			//root.log(multiplier)
+		}
+		else {
+			damageActive = 0;
+		}
+		
+		return damageActive;
 	}
